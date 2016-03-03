370b5df6387030640cd1090bef596f4039f37dd4
Wesley Moy <wesley@nextdoor.com>
2016-03-03 14:47:54 -0800

    Minimally working mouse timer

--- Mouse Timer/ViewController.swift
+++ Mouse Timer/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timetime: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NSTimer.scheduledTimerWithTimeInterval(NSTimeInterval(0.5), target: self, selector: Selector("foo"), userInfo: nil, repeats: true).fire()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func foo() {
+        let components = NSDateComponents()
+        components.year = 2016
+        components.month = 3
+        components.day = 22
+        components.hour = 16
+        components.minute = 45
+        let calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+        let destination = calendar.dateFromComponents(components)!
+        let formatter = NSDateComponentsFormatter()
+        let intervalComponents = calendar.components([.Day, .Hour, .Minute, .Second], fromDate: NSDate(), toDate: destination, options: NSCalendarOptions())
+        let strstr = formatter.stringFromDateComponents(intervalComponents)
+        dispatch_async(dispatch_get_main_queue()) {
+            self.timetime.text = strstr
+        }
+    }
 }
 
